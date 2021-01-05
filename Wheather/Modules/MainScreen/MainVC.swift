dcb0df504c98cd9a21a9eb81e03603d9a9ad5094
Sergey Mikovoz <s.mikovoz@vironit.co.uk>
2021-01-05 15:18:18 +0300

    Add Constants.swift + NetworkService.swift + logic with them

--- Wheather/Modules/MainScreen/MainVC.swift
+++ Wheather/Modules/MainScreen/MainVC.swift
@@ -10,6 +10,31 @@ class MainViewController: UIViewController {
     @IBOutlet private weak var showWeatherButton: UIButton!
     @IBOutlet private weak var annotationLabel: UILabel!
     
+    // MARK: - IBActions
+
+    @IBAction private func tapCityTextField(_ sender: Any) {
+        cityTextField.title = "your city"
+        cityTextField.selectedTitleColor = UIColor(red: 32/255, green: 150/255, blue: 96/255, alpha: 1.0)
+    }
+    @IBAction private func tapShowWeatherButton(_ sender: Any) {
+        cityTextField.title = "your city"
+        cityTextField.selectedTitleColor = UIColor(red: 32/255, green: 150/255, blue: 96/255, alpha: 1.0)
+        guard let cityName = cityTextField.text, cityName.isEmpty != true else {
+            showAlert(with: "Please enter city name")
+            return
+        }
+        if isValidCity(city: cityName) {
+            let url = NetworkService.getURL(url: RequestConstants.url, cityName: cityName, appId: RequestConstants.appId)
+            NetworkService.cityRequest(controller: self, urlString: url) { [weak self] in
+                guard let self = self, let model = $0 else { return }
+                self.showCityTemperature(with: model)
+            }
+        } else {
+            cityTextField.title = "wrong city"
+            cityTextField.selectedTitleColor = .red
+        }
+    }
+    
     // MARK: - LifeCycle
     
     override func viewDidLoad() {
@@ -27,31 +52,7 @@ class MainViewController: UIViewController {
         navigationController?.pushViewController(viewController, animated: true)
     }
     
-    @objc private func isSelectedTextField() {
-        cityTextField.title = "your city"
-        cityTextField.selectedTitleColor = UIColor(red: 32/255, green: 150/255, blue: 96/255, alpha: 1.0)
-    }
-    
-    @objc private func tapShowWeatherButton() {
-        cityTextField.title = "your city"
-        cityTextField.selectedTitleColor = UIColor(red: 32/255, green: 150/255, blue: 96/255, alpha: 1.0)
-        guard let cityName = cityTextField.text, cityName.isEmpty != true else {
-            showAlert(with: "Please enter city name")
-            return
-        }
-        if isValidCity(city: cityName) {
-            let url = getURL(with: cityName)
-            cityRequest(urlString: url) { [weak self] in
-                guard let self = self, let model = $0 else { return }
-                self.showCityTemperature(with: model)
-            }
-        } else {
-            cityTextField.title = "wrong city"
-            cityTextField.selectedTitleColor = .red
-        }
-    }
-
-    private func showAlert(with message: String) {
+    func showAlert(with message: String) {
         let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
         let doneButton = UIAlertAction(title: "done", style: .default, handler: nil)
         alert.addAction(doneButton)
@@ -62,38 +63,10 @@ class MainViewController: UIViewController {
     // MARK: - Setup
     
     private func setupTargets() {
-        cityTextField.addTarget(self, action: #selector(isSelectedTextField), for: .editingChanged)
-        showWeatherButton.addTarget(self, action: #selector(tapShowWeatherButton), for: .touchDown)
-        
+
         cityTextField.textAlignment = .center
         
         cityTextField.delegate = self
-    }
-    
-    // MARK: - API Calls
-    
-    private func getURL(with cityName: String) -> String {
-        return "http://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=302b5b3153622f3d6f42aa61de61076a"
-    }
-    
-    private func cityRequest(urlString: String, completion: @escaping (City?) -> Void) {
-        
-        AF.request(urlString).responseData { response in
-            switch response.result {
-            case .success:
-                do {
-                    guard let data = response.data else { return }
-                    let cities = try JSONDecoder().decode(City.self, from: data)
-                    completion(cities)
-                } catch {
-                    self.showAlert(with: "Sorry, weather in this city is not available")
-                    completion(nil)
-                }
-            case .failure:
-                self.showAlert(with: "Something went wrong, please try again later")
-                completion(nil)
-            }
-        }
     }
     
 }
---
+++ Wheather/Utils/Constants.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+struct RequestConstants {
+    static let url = "http://api.openweathermap.org/data/2.5/weather?q="
+    static let appId = "&appid=302b5b3153622f3d6f42aa61de61076a"
+}
